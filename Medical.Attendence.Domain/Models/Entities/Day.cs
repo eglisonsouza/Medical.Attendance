97fc4e718666bd69839864a362c03b3045d224e6
eglison.souza2019 <eglison.souza2019@gmail.com>
2024-03-14 08:17:24 -0300

    feat: create entities

---
+++ Medical.Attendence.Domain/Models/Entities/Day.cs
@@ -0,0 +1,16 @@
+﻿using Medical.Attendance.Domain.Models.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Medical.Attendance.Domain.Models.Entities
+{
+    public sealed class Day : BaseEntity
+    {
+        public string DayName { get; private set; }
+        public int Sequencial { get; private set; }
+        public List<HourDay> Hours { get; private set; }
+    }
+}
---
+++ Medical.Attendence.Domain/Models/Entities/HourDay.cs
@@ -0,0 +1,10 @@
+﻿using Medical.Attendance.Domain.Models.Entities.Base;
+
+namespace Medical.Attendance.Domain.Models.Entities
+{
+    public sealed class HourDay : BaseEntity
+    {
+        public DateTime Hour { get; private set; }
+        public int Sequencial { get; private set; }
+    }
+}
